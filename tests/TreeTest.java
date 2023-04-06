60df84108ed6bf094cd9f7153e1dfd1d18a60120
Raymond Zhao <raymondzhao304@gmail.com>
2023-04-05 19:00:05 -0700

    ex. 26

--- tests/TreeTest.java
+++ tests/TreeTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TreeTest {
